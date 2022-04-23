dc00362d9763cb1d84adba3b0f164331acb0d60c
cc-jhr <34243889+cc-jhr@users.noreply.github.com>
2022-04-23 13:36:25 +0200

    Added JVM target and publishing plugin

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.21"
     `java-library` // Apply the java-library plugin for API and implementation separation.
+    `maven-publish`
     id("org.jetbrains.dokka") version "1.6.20"
 }
+
+val githubUsername by extra { System.getenv("GH_USERNAME") ?: project.findProperty("GH_USERNAME") as String? ?: "cc-jhr" }
+val githubReleaseToken by extra { System.getenv("GH_PACKAGES_RELEASE_TOKEN") ?: project.findProperty("GH_PACKAGES_RELEASE_TOKEN") as String? ?: "NOT_SET" }
+
+group = "io.github.ccjhr"
+version = project.findProperty("release.version") as String? ?: ""
 
 repositories {
     mavenCentral()
@@ -21,7 +28,12 @@ dependencies {
 
 tasks.withType<Test> {
     useTestNG()
-    maxParallelForks = Runtime.getRuntime().availableProcessors()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 tasks.dokkaHtml.configure {
@@ -35,6 +47,60 @@ tasks.dokkaHtml.configure {
                 "src/samples/kotlin/IntSamples.kt",
                 "src/samples/kotlin/ThrowableSamples.kt",
             )
+        }
+    }
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javaDoc by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = rootProject.name
+            url = uri("https://maven.pkg.github.com/$githubUsername/${rootProject.name}")
+            credentials {
+                username = githubUsername
+                password = githubReleaseToken
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = rootProject.name
+            version = project.version.toString()
+
+            from(components["java"])
+            artifact(sourcesJar.get())
+            artifact(javaDoc.get())
+
+            pom {
+                packaging = "jar"
+                name.set(rootProject.name)
+                description.set("This is an assertion/matcher library for kotlin using infix functions.")
+                url.set("https://github.com/$githubUsername/${rootProject.name}")
+
+                licenses {
+                    license {
+                        name.set("Apache-2.0 License")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git@github.com:$githubUsername/${rootProject.name}.git")
+                    developerConnection.set("scm:git:ssh://github.com:$githubUsername/${rootProject.name}.git")
+                    url.set("https://github.com/$githubUsername/${rootProject.name}")
+                }
+            }
         }
     }
 }
